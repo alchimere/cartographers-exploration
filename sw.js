b53274177c5025e01972cee75588f525fa84bf45
Yann Berthou (firetonton) <firetonton@gmail.com>
2024-07-20 15:47:53 +0000

    Add missing sw.js

---
+++ sw.js
@@ -0,0 +1,54 @@
+const cacheName = "cache1"; // Change value to force update
+
+self.addEventListener("install", event => {
+	// Kick out the old service worker
+	self.skipWaiting();
+
+	event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll([
+				"/",
+				"favicon.ico", // Favicon, IE and fallback for other browsers
+				"favicon-16x16.png", // Favicon, default
+				"favicon-32x32.png", // Favicon, Safari on Mac OS
+				"index.html", // Main HTML file
+				"logo.png", // Logo
+				"main.js", // Main Javascript file
+				"manifest.json", // Manifest file
+				"style.css", // Main CSS file
+			]);
+		})
+	);
+});
+
+self.addEventListener("activate", event => {
+	// Delete any non-current cache
+	event.waitUntil(
+		caches.keys().then(keys => {
+			Promise.all(
+				keys.map(key => {
+					if (![cacheName].includes(key)) {
+						return caches.delete(key);
+					}
+				})
+			)
+		})
+	);
+});
+
+// Offline-first, cache-first strategy
+// Kick off two asynchronous requests, one to the cache and one to the network
+// If there's a cached version available, use it, but fetch an update for next time.
+// Gets data on screen as quickly as possible, then updates once the network has returned the latest data. 
+self.addEventListener("fetch", event => {
+	event.respondWith(
+		caches.open(cacheName).then(cache => {
+			return cache.match(event.request).then(response => {
+				return response || fetch(event.request).then(networkResponse => {
+					cache.put(event.request, networkResponse.clone());
+					return networkResponse;
+				});
+			})
+		})
+	);
+});
