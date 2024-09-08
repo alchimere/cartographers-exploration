4374da531a1ef158847aadda43b412cd413518dd
Yann Berthou <firetonton@gmail.com>
2024-09-08 15:28:42 +0200

    Setup UI

--- sw.js
+++ sw.js
@@ -45,6 +45,7 @@ self.addEventListener("fetch", event => {
 	event.respondWith(
 		caches.open(cacheName).then(cache => {
 			return cache.match(event.request).then(response => {
+				response = null;
 				return response || fetch(event.request).then(networkResponse => {
 					cache.put(event.request, networkResponse.clone());
 					return networkResponse;
